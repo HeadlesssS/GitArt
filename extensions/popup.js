8c0a22817076d2283012cf6e3ac682e38607d863
HeadlesssS <aaryanarju@gmail.com>
2026-06-01 18:33:30 +0545

    quick update: grids

--- extensions/popup.js
+++ extensions/popup.js
@@ -1,12 +1,10 @@
-
-
-
 let grid = [];
 document.getElementById('submit').addEventListener('click', getContributionSize);
 
 function getContributionSize(){
+    console.log("Getting Contribution Size");
     const startDate = document.getElementById('Cdate');
-const timeallocated = document.getElementById('timeframe');
+    const timeallocated = document.getElementById('timeframe');
     
     console.log(startDate.value);
     console.log(timeallocated.value);
@@ -20,13 +18,12 @@ const timeallocated = document.getElementById('timeframe');
 
 }
 
-getContributionSize();
 
 
 function Make2DArray(strtDt,tmAll){
     console.log("Making 2D Array");
     console.log(strtDt);
-    console.log(tmAll);
+    console.log(tmAll); 
     for (let i = 0; i < tmAll; i++){
         grid[i] = [];
         for (let j = 0; j < 7; j++){
@@ -36,10 +33,26 @@ function Make2DArray(strtDt,tmAll){
         }
     } console.log("2D Array Made");
     console.log(grid);
-    return grid;
+
+    draw2DArray(grid);
     }
 
 
-function setup(){
+function draw2DArray(grid){
+    console.log("Drawing 2D Array");
+    const htmltable = document.getElementById('contributionTable');
+    htmltable.innerHTML = ''; 
+
+    for (let i=0; i<grid.length; i++){
+        let row = htmltable.insertRow();
+
+        for (let j = 0; j <grid[i].length;j++){
+            let cell = row.insertCell();
+
+            cell.innerText = grid[i][j];
+        }
+
+    }
+
 
 }
